d4785cacd0bf234e3c362aff430bfe475237b206
Svyatoslav Titov <slava.titoff@yandex.ru>
2018-06-28 16:41:26 +0400

    Init

---
+++ Console/Program.cs
@@ -0,0 +1,43 @@
+﻿using NnackDI;
+using System;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Container.RegistrationType<IFoo, Foo>();
+            Container.RegistrationType<IFooTwo, FooTwo>();
+
+            var getType = Container.ResolveType<IFooTwo>();
+
+            System.Console.ReadKey(true);
+        }
+    }
+
+    #region Test classes
+    public interface IFoo
+    {
+        void PrintAny();
+    }
+    public class Foo : IFoo
+    {
+        public void PrintAny()
+        {
+            System.Console.WriteLine("Hello from Foo class");
+        }
+    }
+
+    public interface IFooTwo { }
+
+    public class FooTwo : IFooTwo
+    {
+        public FooTwo(IFoo foo)
+        {
+            foo.PrintAny();
+        }
+    }
+
+    #endregion
+}
---
+++ NnackDI/Container.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NnackDI
+{
+	public static class Container 
+	{
+        private static Dictionary<Type, object> _commonDictionary = new Dictionary<Type, object>();
+
+        public static void RegistrationType<TType, RType>()
+        {
+            if (_commonDictionary.ContainsKey(typeof(TType)))
+                return;
+
+            _commonDictionary.Add(typeof(TType), typeof(RType));
+        }
+
+        public static void RegistrationInstance<TType>(TType instance)
+        {
+            if (_commonDictionary.ContainsKey(typeof(TType)))
+                return;
+
+            _commonDictionary.Add(typeof(TType), instance);
+        }
+
+        /// <summary>
+        /// Resolve class by the most bigger constructor parameters count
+        /// </summary>
+        /// <typeparam name="TType">Resolved type</typeparam>
+        /// <returns>Instance of TType</returns>
+        public static TType ResolveType<TType> ()
+        {
+            var typeObject = _commonDictionary[typeof(TType)] as Type;
+            var constructors = typeObject.GetConstructors();
+            var mostBiggerConstructor = constructors.OrderByDescending(item => item.GetParameters().Length).First();
+
+            List<Type> constrTypes = new List<Type>();
+            foreach (var parameter in mostBiggerConstructor.GetParameters())
+            {
+                constrTypes.Add(parameter.ParameterType);
+            }
+
+            var ss = constrTypes.Select(item => typeof(Container).GetMethod("ResolveType").MakeGenericMethod(item).Invoke(this, null)).ToArray();
+            var result = (TType)mostBiggerConstructor.Invoke(constrTypes.Select(item => typeof(Container).GetMethod("ResolveType").MakeGenericMethod(item).Invoke(this, null)).ToArray());
+
+            return result;
+        }
+
+        public static TType ResolveInstance<TType>()
+        {
+            var instance = _commonDictionary[typeof(TType)];
+            return (TType)instance;
+        }
+	}
+}
